430477454a04ebbb1e6f3eefaff1da04fa4fb905
trinitario6 <justinsiu6@gmail.com>
2026-03-15 10:32:32 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'seb-blast-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
